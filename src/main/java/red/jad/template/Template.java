9efa2db1fa1fbe41511244cec54c00ea12fcf8f2
jadc <chehimj@gmail.com>
2020-08-06 21:59:31 -0600

    \

---
+++ src/main/java/red/jad/template/Template.java
@@ -0,0 +1,10 @@
+package red.jad.template;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello!");
+	}
+}
