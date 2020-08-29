71ef18ec765f0107685bed45a858cca2eb27a555
jadc <chehimj@gmail.com>
2020-08-29 14:24:25 -0600

    loicense

--- src/main/java/red/jad/template/Template.java
+++ src/main/java/red/jad/template/Template.java
@@ -1,10 +1,19 @@
 package red.jad.template;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
-public class Template implements ModInitializer {
+public class Template implements ModInitializer, ClientModInitializer {
 	@Override
 	public void onInitialize() {
 		System.out.println("Hello!");
 	}
+
+    @Environment(EnvType.CLIENT)
+	@Override
+	public void onInitializeClient(){
+
+    }
 }
